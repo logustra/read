6d9fa89645a01f01e9bfdcaf3f58b6dce088459d
logustra <logustra@gmail.com>
2020-02-18 22:02:18 +0700

    fix: useParams undifined

--- src/modules/Post/views/postDetail.tsx
+++ src/modules/Post/views/postDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Styled from 'styled-components/macro'
 
 import { PostCommentModel } from '../contracts/postDetailContracts'
