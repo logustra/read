fde46132cf9796afdcbfdf1324d30675b847fd54
adibfirman <dib.firman@gmail.com>
2020-02-23 19:24:24 +0700

    chore finish first note

--- src/modules/Post/components/PostList/postList.tsx
+++ src/modules/Post/components/PostList/postList.tsx
@@ -1,61 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Styled from 'styled-components/macro'
+import React from "react";
+import { Link } from "react-router-dom";
+import Styled from "styled-components/macro";
 
-import { PostListModel, PostListProps } from '../../contracts/postListContracts'
+import {
+  PostListModel,
+  PostListProps
+} from "../../contracts/postListContracts";
 
-import { rem } from '@/styles'
+import { rem } from "@/styles";
 
-import { Loading } from 'atoms'
-import { Card } from 'templates'
+import { Loading } from "atoms";
+import { Card } from "templates";
 
-export default function PostList({ withAuthor, data }: PostListProps) {
-  function renderPostAuthor (item: any) {
-    return (
-      <div>
-        Written by 
-        <Link to={`/author/${item.userId}`}>
-          {` ` + item.author.name}
-        </Link>
-      </div>
-    )
-  }
-  
-  function renderPostList () {
-    return data.data.map((item: PostListModel) => (
-      <div key={`post-${item.id}`}>
-        <Card>
+function RenderPostAuthor(item: any) {
+  return (
+    <div>
+      Written by
+      <Link to={`/author/${item.userId}`}>{` ` + item.author.name}</Link>
+    </div>
+  );
+}
+
+function RenderPostList({ withAuthor, data }) {
+  return data.data.map((item: PostListModel) => (
+    <div key={`post-${item.id}`}>
+      <Card>
+        <React.Fragment>
           <Link to={`/post/${item.id}`}>
-            <h3 className="title">
-              {item.title}
-            </h3>
+            <h3 className="title">{item.title}</h3>
           </Link>
 
-          {withAuthor && item.author 
-            ? renderPostAuthor(item)
-            : ''
-          }
+          {withAuthor && item.author ? <RenderPostAuthor {...item} /> : ""}
 
-          <div className="description">
-            {item.body}
-          </div>
-        </Card>
-      </div>
-    ))
-  }
+          <div className="description">{item.body}</div>
+        </React.Fragment>
+      </Card>
+    </div>
+  ));
+}
 
+export default function PostList({ withAuthor, data }: PostListProps) {
   return (
     <StyledPostList>
-      {data.isFetching
-        ? <Loading />
-        : renderPostList()
-      }
+      {data.isFetching ? (
+        <Loading />
+      ) : (
+        <RenderPostList data={data} withAuthor={withAuthor} />
+      )}
     </StyledPostList>
-  ) 
+  );
 }
 
 const StyledPostList = Styled.div`
   .card {
-    margin-bottom: ${rem('16px')};
+    margin-bottom: ${rem("16px")};
   }
-`
+`;
