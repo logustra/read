9a85cc9043b2c9e62dd12bdd8ab9b3f40b77c62b
logustra <logustra@gmail.com>
2020-02-26 14:28:34 +0700

    build: update config typescript

--- babel.config.js
+++ babel.config.js
@@ -12,13 +12,25 @@ module.exports = {
       }
     ],
 
-    '@babel/preset-react',
-    '@babel/preset-typescript'
+    '@babel/preset-typescript',
+    '@babel/preset-react'
   ],
 
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-object-rest-spread',
+    [
+      '@babel/plugin-proposal-decorators', {
+        'legacy': true
+      }
+    ],
+
+    [
+      '@babel/plugin-proposal-class-properties', {
+        'loose': true
+      }
+    ],
+    
     'macros'
   ]
 }
