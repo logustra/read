e9d6e885c6128d285f3423904f7fb82a20ef8556
logustra <logustra@gmail.com>
2020-05-05 11:32:02 +0700

    build(husky): add pre-commit & pre-push

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   hooks: {
+    'pre-commit': 'yarn lint',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'pre-push': 'yarn build'
   }
 }
